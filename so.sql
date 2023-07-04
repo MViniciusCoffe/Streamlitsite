a132576695efa28411d1cb9b7242db126db4d436
Garoto Do Café <104383688+MViniciusCoffe@users.noreply.github.com>
2023-07-04 11:57:44 -0300

    Att

--- so.sql
+++ so.sql
@@ -2,8 +2,6 @@ CREATE TABLE tbUsers(
 	id_aluno 	SERIAL 			PRIMARY KEY,
 	curso 		VARCHAR(1000) 	NOT NULL,
 	matricula 	BIGINT 			NOT NULL,
-	senha		VARCHAR			NOT NULL,
+	senha		VARCHAR(255)	NOT NULL,
 	data_nasc 	DATE
 );
-
-SELECT * FROM tbUsers
